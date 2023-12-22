eea1738950784a24c0ae50872d0aca34d2bac69a
Juancg <mr.juancg@gmail.com>
2023-12-22 17:02:15 +0100

    Base del proyecto

---
+++ src/main/java/com/sounders/sounders/SoundersApplication.java
@@ -0,0 +1,13 @@
+package com.sounders.sounders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sounders/sounders/SoundersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sounders.sounders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
