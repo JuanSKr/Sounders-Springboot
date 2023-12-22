72a4386331405d60ff0e7c277eb6bb7257faf784
Juancg <mr.juancg@gmail.com>
2023-12-22 17:38:14 +0100

    Entidades de "Role" y "User" creadas

--- src/main/java/com/sk/sounders/SoundersApplication.java
+++ src/main/java/com/sk/sounders/SoundersApplication.java
@@ -1,4 +1,4 @@
-package com.sounders.sounders;
+package com.sk.sounders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/sk/sounders/entity/User.java
@@ -0,0 +1,39 @@
+package com.sk.sounders.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")})
+    private List<Role> roles = new ArrayList<>();
+
+}
