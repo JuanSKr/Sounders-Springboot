59b0711743e6b6bb1d12c086ba183409c1528400
Juancg <mr.juancg@gmail.com>
2023-12-23 17:16:28 +0100

    Se ha arreglado el formulario de post para que no colapse con la navbar

--- src/main/java/com/sk/sounders/controller/Crud/PostCrud.java
+++ src/main/java/com/sk/sounders/controller/Crud/PostCrud.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
-
 @Controller
 public class PostCrud {
     @Autowired
