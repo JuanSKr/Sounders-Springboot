21c2c3d7df9a0bda5b1fd3f98223cca71482caa6
Juancg <mr.juancg@gmail.com>
2023-12-30 20:52:26 +0100

    Métodos creados en StorageService

--- src/main/java/com/sk/sounders/storage/StorageService.java
+++ src/main/java/com/sk/sounders/storage/StorageService.java
@@ -1,4 +1,22 @@
 package com.sk.sounders.storage;
 
+import jakarta.annotation.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public interface StorageService {
+
+    void init();
+
+    String store(MultipartFile file, String fileName);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
 }
