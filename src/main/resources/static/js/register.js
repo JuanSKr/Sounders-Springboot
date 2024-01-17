e5dc2b35f78e9e893eb09089c9fc8665e527a6bb
JuanSK <mr.juancg@gmail.com>
2024-01-17 09:28:57 +0100

    Se ha añadido el mostrar/ocultar contraseña al register

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,6 +1,24 @@
+/* /login alert */
+
 if (document.querySelector('.alert-info')) {
     document.getElementById('redirectAlert').style.display = 'block';
     setTimeout(function () {
         window.location.href = '/login';
     }, 3000);
+}
+
+/* Show/Toggle password */
+
+function togglePasswordVisibility(id) {
+    var passwordInput = document.getElementById(id);
+    var passwordIcon = document.querySelector('.' + id + '-icon');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordIcon.classList.remove('fas', 'fa-eye');
+        passwordIcon.classList.add('fas', 'fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        passwordIcon.classList.remove('fas', 'fa-eye-slash');
+        passwordIcon.classList.add('fas', 'fa-eye');
+    }
 }
