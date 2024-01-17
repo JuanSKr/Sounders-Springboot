590e7166d25319f48d7858d6823c8b5c8831d63c
JuanSK <mr.juancg@gmail.com>
2024-01-17 09:23:51 +0100

    Modificacion en register e icono añadido en login
    Se ha cambiado la navbar del regsiter a una que sigue el diseño de la página.
    En el login se ha añadido también un botón para ocultar/mostrar la contraseña

---
+++ target/classes/static/js/login.js
@@ -0,0 +1,13 @@
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('password');
+    var passwordIcon = document.querySelector('.password-icon');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordIcon.classList.remove('fa-eye');
+        passwordIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        passwordIcon.classList.remove('fa-eye-slash');
+        passwordIcon.classList.add('fa-eye');
+    }
+}
